c09530f29221c66f29ec819aac5e596aca34654e
codefuel <codefuel@vicxar.com>
2014-01-12 12:54:52 -0800

    Adding sbt to PATH

--- scala/config.bash
+++ scala/config.bash
@@ -1,5 +1,7 @@
 export SCALA_HOME="/opt/scala"
 export PLAY_HOME="/opt/play"
+export SBT_HOME="/opt/sbt"
 
 conditionally_prefix_path "${SCALA_HOME}/bin"
 conditionally_prefix_path "${PLAY_HOME}"
+conditionally_prefix_path "${SBT_HOME}/bin"
