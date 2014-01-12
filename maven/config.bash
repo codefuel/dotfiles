def1e3486c723caa31922081f28c8ea79ed7dc86
codefuel <codefuel@vicxar.com>
2014-01-12 12:54:20 -0800

    maven env variables and addition maven to PATH

--- maven/config.bash
+++ maven/config.bash
@@ -1,3 +1,5 @@
-export M2_HOME="/opt/maven"
-export M2="$M2_HOME/bin"
+export MVN_HOME="/opt/maven"
+export MVN="$MVN_HOME/bin"
+export PATH=$PATH:/opt/maven/apache-maven-3.0.3/bin
+conditionally_prefix_path "$MVN"
 
