c39a784541ba979700e37d6595d6e2affdfcf19c
Aaron Sargent <aaron.sargent@shiftboard.com>
2016-11-18 15:40:08 -0800

    Add tag_release alias for git

--- git/aliases.bash
+++ git/aliases.bash
@@ -32,6 +32,7 @@ alias grc="git rebase --continue"
 alias grs="git rebase --skip"
 alias lg1="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 alias lg2="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+alias tag_release='basetag=$(date +"release-%Y%m%d"); count=$((git tag | grep -c $basetag ) || true); ((++count)); TAG="${basetag}-$count"; git tag $TAG; git push --tag'
 
 case $OSTYPE in
   linux*)
